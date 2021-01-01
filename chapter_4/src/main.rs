e48a6b3e333839b7053d84abf5e23b2d8a4e7429
pr0found <runqiang.crq@alibaba-inc.com>
2021-01-01 22:31:29 +0800

    avoid dangle

--- chapter_4/src/main.rs
+++ chapter_4/src/main.rs
@@ -1,13 +1,8 @@
 fn main() {
-	let mut s1 = String::from("hello");
-	let r1 = &mut s1;
-	let r2 = &mut s1;
-	change(r1);
-	println!("{}", s1);
-	change(r2);
-	println!("{}", s1);
+	let reference_to_nothing = dangle();
 }
 
-fn change(s: &mut String) {
-	s.push_str("world");
+fn dangle() -> &String {
+	let s = String::from("hello");
+	&s
 }
