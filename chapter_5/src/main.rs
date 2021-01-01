22387e24ee08727207fc0dca0539e9bc6fd657af
pr0found <runqiang.crq@alibaba-inc.com>
2021-01-01 23:37:05 +0800

    lifetime check on struct

--- chapter_5/src/main.rs
+++ chapter_5/src/main.rs
@@ -1,3 +1,15 @@
+struct User{
+	name: &str,
+	email: &str,
+	sign_in_count: u64,
+	active: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+	let user1 = User{
+		email: "pr0found@example.com",
+		username: "pr0found",
+		active: true,
+		sigh_in_count: 1,
+	};
 }
---
+++ chapter_5/target/release/deps/chapter_5.d
@@ -0,0 +1,5 @@
+/usr/share/workspace/rust-project/chapter_5/target/release/deps/chapter_5: src/main.rs
+
+/usr/share/workspace/rust-project/chapter_5/target/release/deps/chapter_5.d: src/main.rs
+
+src/main.rs:
