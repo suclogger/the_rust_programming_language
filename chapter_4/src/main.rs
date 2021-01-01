23081d900d6bb416ece8fb5471c431c7b84fcbbe
pr0found <runqiang.crq@alibaba-inc.com>
2021-01-01 17:46:07 +0800

    reference not allow change

--- chapter_4/src/main.rs
+++ chapter_4/src/main.rs
@@ -1,10 +1,8 @@
 fn main() {
 	let s1 = String::from("hello");
-	let len = calculate_length(&s1);
-	println!("The length of '{}' is {}", s1, len);
+	change(&s1);
 }
 
-fn calculate_length(s: &String) -> usize {
-	let length = s.len();
-	length
+fn change(s: &String) {
+	s.push_str(", world");
 }
