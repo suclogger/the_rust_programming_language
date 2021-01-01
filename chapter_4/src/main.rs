05328df6c85bf81191e4e637a4821230c95cd20c
pr0found <runqiang.crq@alibaba-inc.com>
2021-01-01 17:32:16 +0800

    value moved error for string

--- chapter_4/src/main.rs
+++ chapter_4/src/main.rs
@@ -1,9 +1,12 @@
 fn main() {
 	let s = String::from("hello");
 	takes_ownership(s);
-	
+	// this would produce error
+	println!("{}", s);	
 	let x = 5;
 	makes_copy(x);
+	// this is good to go
+	println!("{}", x);
 }
 
 fn takes_ownership(some_string: String) {
