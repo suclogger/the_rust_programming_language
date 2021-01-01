01e5bbf8907f0eb778b52909fe34e5934a11d2b3
pr0found <runqiang.crq@alibaba-inc.com>
2021-01-02 02:00:10 +0800

    print trait

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
 	width: u32,
 	height: u32,
@@ -6,11 +7,7 @@ struct Rectangle {
 fn main() {
 	let rect1 = Rectangle{width : 30, height: 50};
 	println!(
-		"The area of the rectangle is {} square pixels",
-		area(&rect1)
+		"{:?}",
+		rect1
 	)
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-	rectangle.width*rectangle.height
 }
