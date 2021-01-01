81d13bec597ea2145df08443090644a1be472c2d
pr0found <runqiang.crq@alibaba-inc.com>
2021-01-01 17:51:36 +0800

    second mutable borrow not allowed

--- chapter_4/src/main.rs
+++ chapter_4/src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
 	let mut s1 = String::from("hello");
-	change(&mut s1);
+	let r1 = &mut s1;
+	let r2 = &mut s1;
+	change(r1);
+	println!("{}", s1);
+	change(r2);
 	println!("{}", s1);
 }
 
 fn change(s: &mut String) {
-	s.push_str(", world");
+	s.push_str("world");
 }
