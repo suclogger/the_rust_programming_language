f1e227ef9fa1248a129ef3aa3727d77fcce82910
pr0found <runqiang.crq@alibaba-inc.com>
2021-01-01 17:44:14 +0800

    use reference

--- chapter_4/src/main.rs
+++ chapter_4/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
 	let s1 = String::from("hello");
-	let (s2, len) = calculate_length(s1);
-	println!("The length of '{}' is {}", s2, len);
+	let len = calculate_length(&s1);
+	println!("The length of '{}' is {}", s1, len);
 }
 
-fn calculate_length(s: String) -> (String, usize){
+fn calculate_length(s: &String) -> usize {
 	let length = s.len();
-	(s, length)
+	length
 }
